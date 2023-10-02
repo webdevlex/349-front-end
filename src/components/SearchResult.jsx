1bc163b9d40fd2261a116fb1f6bf3a7584aa7fd7
jpope6 <imthepope@csu.fullerton.edu>
2023-10-01 20:15:58 -0700

    Added a modal when you click on a movie

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
+
+Modal.setAppElement('#root');
 
 const genres = {
 	28: "Action",
@@ -23,26 +26,55 @@ const genres = {
 };
 
 const SearchResult = ({ movie }) => {
+	const [showModal, setShowModal] = useState(false);
+
+	const handleModalOpen = () => {
+		setShowModal(true);
+	}
+
+	const handleModalClose = () => {
+		setShowModal(false);
+	}
+
 	return (
 		<div className="result">
 			<img
 				loading="lazy"
 				src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
 				alt=""
+				onClick={handleModalOpen}
 			/>
 			<p>Title: {movie.original_title}</p>
-			<p>Description: {movie.overview}</p>
-			<p>
-				Genres:{" "}
-				{movie.genre_ids.map((id) => (
-					<span key={id}>{genres[id]}, </span>
-				))}
-			</p>
-			<p>Popularity: {movie.popularity}</p>
-			<p>Release: {movie.release_date}</p>
 			<p>Vote Average: {movie.vote_average}</p>
 			<p>Vote Count: {movie.vote_count}</p>
-		</div>
+
+			<Modal
+				isOpen={showModal}
+				className="result-modal"
+				style={{
+					content: {
+						backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), 
+								url('https://image.tmdb.org/t/p/original/${movie.backdrop_path}')`,
+						backgroundSize: 'cover',
+					},
+					overlay: {
+						background: 'rgba(0, 0, 0, 0.7)',
+					},
+				}}
+			>
+				<p>Title: {movie.original_title}</p>
+				<p>Description: {movie.overview}</p>
+				<p>
+					Genres:{" "}
+					{movie.genre_ids.map((id) => (
+						<span key={id}>{genres[id]}, </span>
+					))}
+				</p>
+				<p>Popularity: {movie.popularity}</p>
+				<p>Release: {movie.release_date}</p>
+				<button onClick={handleModalClose}>Close Modal</button>
+			</Modal>
+		</div >
 	);
 };
 
